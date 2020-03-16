e993e7790dd04948dee024743be92945aa58fd08
AndChen153 <achen1192@gmail.com>
2020-03-16 17:17:37 -0500

    fixed pixel

--- neopixeltest.py
+++ neopixeltest.py
@@ -3,12 +3,12 @@ import neopixel
 import time
 
 
-pixels = neopixel.NeoPixel(board.D18, 2, pixel_order=neopixel.RGB)
+pixels = neopixel.NeoPixel(board.D18, 2, pixel_order=neopixel.RGBW)
 
 RED = (0,255,0,0)
 BLUE = (0,0,255,0)
 GREEN = (255,0,0,0)
-#WHITE = (0,0,0,255)
+WHITE = (0,0,0,255)
 
 def showRed():
 	pixels.fill(RED)
@@ -35,8 +35,8 @@ while True:
 	a = int(input(""))
 	b = int(input(""))
 	c = int(input(""))
-	#d = int(input(""))
-	color = (a,b,c)
+	d = int(input(""))
+	color = (a,b,c,d)
 	pixels.fill(color)
 	pixels.show()
 	time.sleep(0.5)
