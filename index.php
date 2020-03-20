5f0804053146d09ad144e469920d589c4cad92b5
AndChen153 <achen1192@gmail.com>
2020-03-19 22:16:36 -0500

    website test 4

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
     <p><a href="https://www.google.com/search?q=rgb+color+picker&oq=rgb+color+picker&aqs=chrome.0.0l8.1775j0j7&sourceid=chrome&ie=UTF-8">Easy RBG color picker</a></p>
 
     <?php
-        shell_exec ("sudo python /home/pi/lightWebsite/neopixeltest.py '".$green."' '".$red."' '".$blue."'");
+        shell_exec ("sudo python /home/pi/lightWebsite/neopixeltest.py True '".$green."' '".$red."' '".$blue."'");
     ?>
 
     <form action="" method="post">
