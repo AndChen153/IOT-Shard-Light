175a405d38c1528092afa246fab1ad458ac1a087
AndChen153 <achen1192@gmail.com>
2020-03-19 21:59:41 -0500

    website test 4

--- action.php
+++ action.php
@@ -1,3 +1,10 @@
+<html>
+<body>
+
+
 <?php
     shell_exec ("sudo python /home/pi/lightWebsite/neopixeltest.py '".$_GET["greenAMT"]."' '".$_GET["redAMT"]."' '".$_GET["blueAMT"]."'");
 ?>
+
+</body>
+</html>
