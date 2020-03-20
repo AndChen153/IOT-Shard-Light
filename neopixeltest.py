e34f4ac78654e6ef97dc0a04dc7ebc8e6c33419d
AndChen153 <achen1192@gmail.com>
2020-03-19 21:07:20 -0500

    website test 2

--- neopixeltest.py
+++ neopixeltest.py
@@ -5,7 +5,7 @@ import sys
 
 pixels = neopixel.NeoPixel(board.D18, 2, pixel_order=neopixel.RGBW)
 
-RED = (0,255,0,0)
+RED = (1,1,1,1)
 BLUE = (0,0,255,0)
 GREEN = (255,0,0,0)
 WHITE = (0,0,0,255)
@@ -29,8 +29,47 @@ def showWWhite():
 	pixels.sshow()
 
 
+
+pixels.fill(RED)
+pixels.show()
+time.sleep(0.1)
+
+
+#arg one is on/off 
+#for if there are no rgb arguments
+if len(sys.argv)==2 and sys.argv[1]:
+	print("1")
+	pixels.fill((255,255,255,0))
+	pixels.show()
+	time.sleep(0.5)
+	#args 3-5 are red green blue
+elif len(sys.argv)==5 and sys.argv[1]:
+	print("2")
+	R = int(sys.argv[2])
+	G = int(sys.argv[3])
+	B = int(sys.argv[4])
+	COLOR = (R,G,B,0)
+	pixels.fill(COLOR)
+	pixels.show()
+	time.sleep(0.5)
+else:
+	print("3")
+	pixels.fill(OFF)
+	pixels.show()
+	time.sleep(0.5)
+
+
+
+
+
+
+'''
 while True:
-	'''
+	
+	pixels.fill(RED)
+	pixels.show()
+	time.sleep(0.1)
+	
 	#arg one is on/off 
 	#for if there are no rgb arguments
 	if len(sys.argv)==2 and sys.argv[1]:
@@ -53,14 +92,11 @@ while True:
 		pixels.fill(OFF)
 		pixels.show()
 		time.sleep(0.5)
-	'''
 
 	
-	'''
 	pixels.fill(RED)
 	pixels.show()
 	time.sleep(0.5)
-	'''
 	
 	pixels.fill(GREEN)
 	pixels.show()
@@ -69,5 +105,6 @@ while True:
 	pixels.fill(WHITE)
 	pixels.show()
 	time.sleep(0.5)
+'''
 	
 	
