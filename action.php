577e7bbca2bed4f7c79a524f2c957e0680a1ea6d
AndChen153 <achen1192@gmail.com>
2020-03-19 21:20:27 -0500

    website test 3

---
+++ action.php
@@ -0,0 +1,4 @@
+<p>R value <input type="text" name="R" /></p>
+        <p>G value <input type="text" name="G" /></p>
+        <p>B value <input type="text" name="B" /></p>
+        <p><input type="submit" /></p>
--- index.php
+++ index.php
@@ -27,11 +27,6 @@
           
         <input type="submit" name="button2"
                 value="Button2"/> 
-
-        <p>R value <input type="text" name="R" /></p>
-        <p>G value <input type="text" name="G" /></p>
-        <p>B value <input type="text" name="B" /></p>
-        <p><input type="submit" /></p>
     </form> 
 
 	 
